a00989631fce23c69407d2514afbd06a803c1322
juraj_slk <juraj_slk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-06 08:54:44 +0000

    First svn commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/clearcacheextend/trunk@9881 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "clearcacheextend"
+#
+# Auto generated 04-03-2008 11:10
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Extended TCEMAIN.clearCacheCmd command',
+	'description' => 'Extend TCEMAIN.clearCacheCmd width some userfull options like using of aliases, clearing whole branches, clearing pages on the ground of their content etc.',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '0.1.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Juraj Sulek',
+	'author_email' => 'juraj@sulek.sk',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"1664";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_clearcacheextend.php";s:4:"9fb2";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"b16e";s:13:"locallang.php";s:4:"6dec";s:14:"doc/manual.sxw";s:4:"8792";s:19:"doc/wizard_form.dat";s:4:"887e";s:20:"doc/wizard_form.html";s:4:"ed46";}',
+);
+
+?>
